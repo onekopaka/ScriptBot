a96b28903e846a42631093921b9efa1364420b45
amcbain <amcbain@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2008-07-04 21:20:21 +0000

    Added help into to reload.js

--- src/plugins/reload.js
+++ src/plugins/reload.js
@@ -1,3 +1,16 @@
+/**************************************************
+* A test plugin to reload specified files. The    *
+* filename given is automatically assumed to be   *
+* ".js", so the extension should be omitted. In   *
+* order for this plugin to work fully (and not    *
+* just load a plugin twice) the specified plugin  *
+* must have a line to unregister any previous     *
+* copies of the plugin. This can be done by       *
+* attaching an id to teh plugin and removing it   *
+* by id in the first line of the file (which will *
+* do nothing if it isn't loaded.                  *
+**************************************************/
+
 // Reloads the specified plugin.
 core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 	var msg = args[args.length-1].toLowerCase();
