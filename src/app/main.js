2f55bc4d9b37997891d87256faeb1f56468258c5
amcbain <amcbain@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2008-08-15 20:07:00 +0000

    Added a config file so that you no longer have to hack main.js

--- src/app/main.js
+++ src/app/main.js
@@ -6,23 +6,41 @@ print("IRC     - use the quit command to close nicely\n");
 // Create a new PircBot instance and start it up.
 var core = createBot();
 
+// Get the config file contents.
+var config = JSON.parse(IO.readFile("config.txt"));
+core.password = config.password || "";
+
+// Load any plugins before be join IRC
+if(config.plugins) {
+	for(var i = 0; i < config.plugins.length; i++) {
+		try {
+			IO.include("plugins/" + config.plugins[i] + ".js");
+		} catch(e) {
+			print("'" + config.plugins[i] + "' failed to load with this error:");
+			print(e);
+		}
+	}
+}
+
 // Connecting to the server sometimes fails.
 try {
-	//core.initialize("ScriptBot", "irc.freenode.net", "#pircbot");
-//	core.initialize("ScriptBot", "irc.mozilla.org", "#bots-test");
+	if(!config.name) throw "Cannot connect to IRC, no bot name specified. Check config file.";
+	if(!config.server) throw "Cannot connect to IRC, no server specified. Check config file.";
+	if(!config.channels) throw "Cannot connect to IRC, no bot name listed. Check config file.";
+	if(config.channels.length === 0) throw "Must specify at least one channel to join IRC. Check config file.";
+
+	// Join IRC.
+	core.initialize(config.name, config.server);
+
+	// Join any specified channels.
+	var keys = config.keys || [];
+	for(var i = 0; i < config.channels.length; i++) {
+		core.getBot().joinChannel(config.channels[i], keys[i] || "");
+	}
 } catch(e) {
 	print(e);
 	print("Program terminated.");
 	System.exit(0); // Just in case.
 }
 
-// Yeah ... the bot doesn't do this for us. Could present problems if it did.
-core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
-	if(args[args.length-1] === "quit") bot.exit();
-});
-core.registerPluginInfo("quit", function(bot, event, args, priv) {
-	bot.sendMessage(args[0], "quit: Force me to leave IRC.");
-});
 
-// Load our test plugins.
-IO.include("plugins/initplugins.js");
