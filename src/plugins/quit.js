66edbd267625cbacbea674e0d77134d86460f657
amcbain <amcbain@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2008-08-20 00:13:30 +0000

    committed remaining stuff.

--- src/plugins/quit.js
+++
@@ -1,7 +0,0 @@
-// Yeah ... the bot doesn't do this for us. Could present problems if it did.
-core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
-	if(args[args.length-1] === "quit") bot.exit();
-});
-core.registerPluginInfo("quit", function(bot, event, args, priv) {
-	bot.sendMessage(args[0], "quit: Force me to leave IRC.");
-});
