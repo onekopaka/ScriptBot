b859664be0da195fd29d4677e4627025d373a131
amcbain <amcbain@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2008-09-05 23:40:22 +0000

    Finished the time-plus plugin.

--- xplugins/time-plus.js
+++ xplugins/time-plus.js
@@ -1,30 +1,34 @@
 /************************************************
-* A simple time plugin. It responds to "time"   *
+* A simple time plugin. It responds to "wtime"  *
 * and returns the current time of the machine   *
 * on which the bot is being run. Alternatively, *
 * a place can be specified, and the bot will    *
-* ask Google for the current time in the        *
-* location and return it.                       *
+* ask worldtimeserver.com for the current time  *
+* in the location and return it.                *
 ************************************************/
 
 // Remove any other "time" plugins.
-core.unregisterPluginByEvent(Event.MESSAGE, "time");
+core.unregisterPluginByEvent(Event.MESSAGE, "wtime");
 // Reads off the current time somewhere.
 core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 	var msg = args[args.length-1].toLowerCase();
 
 	// Only respond to "time" messages.
-	if(msg.substring(0, 5) === "time") {
-		msg = msg.substring(5).replace(/^\s+/,"").replace(/\s+$/,"");
-print(msg);
+	if(msg.substring(0, 5) === "wtime") {
+		msg = Util.trim(msg.substring(5));
+
 		// Handle bot-local time; otherwise foreign time.
-		if(msg === "") {
-			var date = new Date();
-			bot.sendMessage(args[0], "It is now " + date.getHours() + ":" + date.getMinutes());
-		} else {
-			bot.sendMessage(args[0], "Current time in " + msg + " is ");
+		if(msg !== "") {
+			var result = IO.fetchURL("http://www.worldtimeserver.com/search.aspx?searchfor=" + msg).replace(/\n/g,"");
+			result = /<div id="analog-digital">\s+<span class="font7">(.*?)<\/span>\s+<\/div>/.exec(result);
+			if(result !== null) {
+				result = Util.trim(result[1]);
+				bot.sendMessage(args[0], "The current time in " + msg + " is " + result + ". (www.worldtimeserver.com/search.aspx?searchfor=" + msg + ")");
+			} else {
+				bot.sendMessage(args[0], "Could not get the current time for " + msg + ".");
+			}
 		}
 
 		return true;
 	}
-}, "time");
+}, "wtime");
