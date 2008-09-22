c8a442b42e367c9da39799eca4c219a812447982
onekopaka <onekopaka@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2008-09-22 01:51:47 +0000

    switch to read from config file.

--- xplugins/bugzilla.js
+++ xplugins/bugzilla.js
@@ -8,11 +8,15 @@ core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 	var msg = args[args.length-1].split(/\s+/g);
 	if(msg[0] === "bug") {
 		// A bug? Get it!
-		// For sample, we use OKS Bugzilla. DO NOT have trailing slash.
-		var bugzilla = "http://oks.verymad.net/~onekopaka/bugzilla";
+		// Read the config for the bugzilla URL. If the config entry doesn't exist, use OKS Bugzilla.
+		if(config.bugzillaurl != undefined) {
+			var bugzilla = config.bugzillaurl;
+		} else {
+			var bugzilla = "http://oks.verymad.net/~onekopaka/bugzilla"
+		}
 		// Construct URL for xml
 		var url = bugzilla + "/show_bug.cgi?ctype=xml&excludefield=long_desc&id=" + msg[1];
-		var xml = IO.fetchURL(url, true);
+		//var xml = IO.fetchURL(url, true);
 		// Construct URL for sending..
 		var bugurl = bugzilla + "/show_bug.cgi?id=" + msg[1];
 		bot.sendMessage(args[0], args[1] + ": "  + bugurl);
