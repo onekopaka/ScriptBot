6b9ffd7225e14bdd9a3a71e4d4312b59a8909567
amcbain <amcbain@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2008-10-09 01:02:54 +0000

    Added aplugin to report unhandled events to IRC. Very simplisitic.

---
+++ src/plugins/unhandled.js
@@ -0,0 +1,9 @@
+/**********************************
+* A simple plugin which points    *
+* out of a message wasn't handled *
+* handled by the bot.             *
+**********************************/
+
+core.registerUnhandledEventPlugin(Event.MESSAGE, function(bot, event, args, priv) {
+	bot.sendMessage(args[0], "I don't know anything about '" + args[args.length-1].substring(0, Math.min(args[args.length-1].length, 20)) + "', " + args[1]);
+});
