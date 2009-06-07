3b440082ef40f840fd9e9ecc2e9b37d1a63e564a
(no author) <(no author)@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-06-07 22:47:11 +0000

    add tropical.js to xplugins

---
+++ xplugins/tropical.js
@@ -0,0 +1,16 @@
+core.unregisterPluginByEvent(Event.MESSAGE, "tropical");
+core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
+	var msg = args[args.length-1].split(/\s+/g);
+	if (msg[0] === "tropical") {
+		// Get tropical weather XML URL
+		var get_content = IO.fetchURL('http://rss.wunderground.com/auto/rss_full/tropical/index.xml?basin=at');
+		var message = '';
+		// Grab a text from that url if it is match
+		for (var i = 0; i < get_content.length && !message; i++) {
+			message = get_content.match(/<description><![CDATA[(.*)]]<\/description>/)[1].replace(/<\/?[^>]+(>|$)/g,"");
+		}
+		// Display it on the channels
+		bot.sendMessage(message);
+	}
+	return true;
+}, "tropical");
