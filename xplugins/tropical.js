2dc02956826c27db26dfa844359d3b96d368969b
onekopaka <onekopaka@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-06-08 04:50:28 +0000

    let tropical.js be a working XML parse demo!

--- xplugins/tropical.js
+++ xplugins/tropical.js
@@ -3,9 +3,9 @@ core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 	var msg = args[args.length-1].split(/\s+/g);
 	if (msg[0] === "tropical") {
 		// Get tropical weather XML loaded into DOM
-		window.location = 'http://rss.wunderground.com/auto/rss_full/tropical/index.xml?basin=at';
+		window.setLocation = 'http://rss.wunderground.com/auto/rss_full/tropical/index.xml?basin=at';
 		stormstatus = document.getElementsByTagName("description");
-		message = stormstatus
+		message = stormstatus[1].innerHTML;
 		// Display it on the channels
 		bot.sendMessage(args[0], args[1] + ": " + message);
 	}
