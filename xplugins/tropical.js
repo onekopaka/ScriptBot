0c0b2368991351dbf9d7a1a943aad42154e82b9b
jonathans <jonathans@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-06-09 01:14:46 +0000

    fixed window line and clean up

--- xplugins/tropical.js
+++ xplugins/tropical.js
@@ -3,17 +3,11 @@ core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 	var msg = args[args.length-1].split(/\s+/g);
 	if (msg[0] === "tropical") {
 		// Get tropical weather XML loaded into DOM
-		window.setLocation = 'http://rss.wunderground.com/auto/rss_full/tropical/index.xml?basin=at';
-		// Find if there's a storm.
-		stormstatus = document.getElementsByTagName("description")[1].innerHTML;
-		if (stormstatus) {
-			// Clean up message.
-			message = stormstatus.replace(/<\/?[^>]+(>|$)/g,"");
-			// Display it on the channel
-			bot.sendMessage(args[0], args[1] + ": " + message);
-		} else {
-			bot.sendMessage(args[0], args[1] + ":" + IO.fetchURL('http://rss.wunderground.com/auto/rss_full/tropical/index.xml?basin=at'));
-		}
+		window.location = 'http://rss.wunderground.com/auto/rss_full/tropical/index.xml?basin=at';
+		// Find if there's a storm then clean up message.
+		message = document.getElementsByTagName("description")[1].innerHTML.replace(/<\/?[^>]+(>|$)/g,"");
+		// Display it on the channel
+		bot.sendMessage(args[0], args[1] + ": " + message);
 	}
 	return true;
 }, "tropical");
