c1a7175c8da00ff5fac05f17fe96790c6256bc88
onekopaka <onekopaka@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-12-04 05:16:11 +0000

    Josh's Learning plugin.

---
+++ xplugins/learn.js
@@ -0,0 +1,49 @@
+/*
+Title: Scriptbot Learning script
+Version: 1.0
+Author: Joshua Merrell <joshuamerrell@gmail.com>
+Contributors: Darren VanBuren <onekopaka@gmail.com>
+Licensed under GPL.
+
+KNOWN BUGS: Currently, if there is no entry for a query, it says <query> is undefined instead of I have no idea what <query> is.
+*/
+core.unregisterPlugin("learn");
+var brain = {};
+var tempBrain = {};
+
+if(IO.objectExists("brain")) {
+			brain = IO.readObject("brain");
+		}
+
+core.registerUnrestrictedPlugin(Event.MESSAGE, function(bot, event, args, priv) {
+
+	var msg = args[args.length-1];
+
+
+	if(msg !== null && msg.length > 0 && msg.search(" is ") > -1 && msg.search("what is") <0)
+	{
+
+		tempBrain[0] = msg.substring(0,msg.indexOf(" is")).toLowerCase();
+		tempBrain[1] = msg.substring(msg.indexOf("is ")+3, msg.length).toLowerCase();
+		brain[tempBrain[0]] = tempBrain[1];
+		//bot.sendMessage(args[0], "brain[0] = " +brain[0]);
+		//bot.sendMessage(args[0], "brain[1] = " +brain[1]);
+		bot.sendMessage(args[0], "I now know that " + tempBrain[0] + " is " + tempBrain[1]);
+		IO.writeObject("brain", brain);
+
+	}
+	
+
+	if(msg !== null && msg.length > 0 && msg.search("what is") > -1)
+		{
+			var lookup = msg.substring(msg.indexOf("is ")+3, msg.length).toLowerCase();
+			if(lookup !== undefined) {
+				bot.sendMessage(args[0], args[1] + ": " + lookup + " is " + brain[lookup]);
+			} else {
+				bot.sendMessage("I have no idea what " + lookup + " is.");
+			}
+	}
+
+
+	return true;
+}, "learn");
