74c6a82856d492ba5ceef33f9aff3aa7e8aeb219
eggbertx <eggbertx@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-12-21 04:18:26 +0000

    Errors array

---
+++ src/plugins/errors.js
@@ -0,0 +1,22 @@
+/*
+Title: Scriptbot Error Array
+Description: This script never directly gives output. It just creates an array with errors.
+Version: 1.1.0.2009.12.20
+Author: Joshua Merrell <joshuamerrell@gmail.com>
+Licensed under GPL.
+*/
+
+core.unregisterPlugin("errors");
+core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
+var msg = args[args.length-1];
+
+var errors = new Array();
+errors[0] = "Error: You do not have the permissions to edit user permissions";
+errors[1] = "Error: No permissions assigned to that user.";
+errors[2] = "Error: You do not have the permissions to unlock this variable";
+errors[3] = "Error: variable does not exist";
+errors[4] = "Error: You do not have the permissions to lock this variable";
+errors[5] = "I have no idea what that is";
+errors[6] = "You do not have the permissions to edit this key";
+
+}, "errors");
