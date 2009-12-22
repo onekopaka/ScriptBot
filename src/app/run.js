1c8574d041598d307094507883ef4b4f4fd077b6
eggbertx <eggbertx@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-12-22 03:10:46 +0000

    Removed IO.Include(dom.js): (dom.js no longer exists

--- src/app/run.js
+++ src/app/run.js
@@ -69,7 +69,6 @@ if(IO.path.lastIndexOf(IO.slash) !== IO.path.length) {
 
 IO.include("app/io.js");
 IO.include("app/JSON.js");
-IO.include("app/dom.js");
 IO.include("app/util.js");
 IO.include("app/timer.js");
 IO.include("app/scriptbot.js");
