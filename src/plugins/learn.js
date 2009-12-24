2cc7119c75496823781d0fdc7cf041be6c3c3511
eggbertx <eggbertx@bbfb6c9c-1aa5-4725-a35c-bad36a758cac>
2009-12-24 05:19:58 +0000

    Fixed perms =D

--- src/plugins/learn.js
+++ src/plugins/learn.js
@@ -1,14 +1,17 @@
 /*
 Title: Scriptbot Learning script
-Version: 1.7.2.2009.12.22
+Version: 2.0.1.2009.12.23
 Author: Joshua Merrell <joshuamerrell@gmail.com>
-Contributors: Darren VanBuren <onekopaka@gmail.com>
-Licensed under GPL.
+Contributors: Darren VanBuren <onekopaka@gmail.com>, Nareshkumar Rao A/L A Achutha Rao <inaresh.online@gmail.com>
+Changes: FINALLY FIXED PERMS =D =D =D
+
 */
 core.unregisterPlugin("learn");
 var brain = {};
 var tempBrain = {};
+var perms = {};
 
+perms = IO.readObject("perms");
 
 if(IO.objectExists("brain")) 
 	{
@@ -19,54 +22,58 @@ else
 	IO.writeObject("brain",brain);
 	}
 
+
 core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 
-	var msg = args[args.length-1];
-
-	//take input with <variable> is <key>
-
-	
-	
-	if(msg.search("who is ") < 0 && msg.search(" is ") > -1 && msg.search("what is") <0 && args[1] != config.name)
+function writevar()
 	{
-		try
-			{
-			brain = IO.readObject("brain");
-			tempBrain[0] = msg.substring(0,msg.indexOf(" is")).toLowerCase();
-			tempBrain[1] = msg.substring(msg.indexOf("is ")+3, msg.length).toLowerCase();
-
-			if(brain[tempBrain[0]].indexOf("+islocked") > 1 && perms[args[1]] !=1)
-				{
-				if(perms[args[1]] !=1)
-					{
-					bot.sendMessage(args[0], "You don't have the permissions to edit "+tempbrain[0]);
-					}
-				}
-			
-			else
-				{
-				brain[tempBrain[0]] = tempBrain[1];
-				IO.writeObject("brain", brain);
-				//bot.sendMessage(args[0], "brain[tempBrain[0]] = " +brain[tempBrain[0]]); //for debugging 
-				//bot.sendMessage(args[0], "brain[tempBrain[1]] = " +brain[tempBrain[1]]); //for debugging
-				var tempDescription = tempBrain[1];
-				tempDescription = tempDescription.replace("+islocked", "");
-				bot.sendMessage(args[0], "I now know that " + tempBrain[0] + " is " + tempDescription);
-				
-				}
-			}
-
-		catch(e)
-			{
-				brain[tempBrain[0]] = tempBrain[1];
-				IO.writeObject("brain", brain);
-				var tempDescription = tempBrain[1];
-				tempDescription = tempDescription.replace("+islocked", "");
-				bot.sendMessage(args[0], "I now know that " + tempBrain[0] + " is " + tempDescription);
-			}
+	brain[tempBrain[0]] = tempBrain[1];
+	IO.writeObject("brain", brain);
+	var tempDescription = tempBrain[1];
+	tempDescription = tempDescription.replace("+islocked", "");
+	bot.sendMessage(args[0], "I now know that " + tempBrain[0] + " is " + tempDescription);
 	}
 
-	else if(msg.search("what is ") > -1)
+	var msg = args[args.length-1];
+	tempBrain[0] = msg.substring(0,msg.indexOf(" is")).toLowerCase();
+	tempBrain[1] = msg.substring(msg.indexOf("is ")+3, msg.length).toLowerCase();
+
+	if(msg.search(" is ") > -1 && msg.indexOf("what is") <0 && msg.indexOf("who is ") < 0 && args[1] != config.name)
+		{
+		if(brain[tempBrain[0]] == undefined)
+			{
+			bot.sendMessage(args[0], "doesn't exist");
+			writevar();
+			}
+			
+		if(brain[tempBrain[0]] != undefined && brain[tempBrain[0]].indexOf("+isLocked") < 0) //if its not locked
+				{
+				bot.sendMessage(args[0], "not locked");
+				writevar();
+				}
+
+		else if(brain[tempBrain[0]] && brain[tempBrain[0]].indexOf("+isLocked") > 1) //if its there and locked
+			{
+			if(perms[args[1]] == undefined) //perms not accepted
+				{
+				bot.sendMessage(args[0], "You don't have the permissions to edit "+tempBrain[0]);
+				}
+			else if(perms[args[1]] == 0)
+				{
+				bot.sendMessage(args[0], "You don't have the permissions to edit "+tempBrain[0]);
+				}
+			else if(perms[args[1]] == 1)   // if there are perms
+				{
+				bot.sendMessage(args[0], "perms = " +perms + perms[0] + perms[1]);
+				bot.sendMessage(args[0], "perms[args[1]] = " +perms[args[1]]);
+				bot.sendMessage(args[0], "locked. perms accepted");
+				writevar();
+				}
+			}
+		}
+	
+
+	else if(msg.indexOf("what is ") > -1)
 		{
 		try
 			{
@@ -82,7 +89,7 @@ core.registerPlugin(Event.MESSAGE, function(bot, event, args, priv) {
 			}
 		}
 
-	else if(msg.search("who is ") > -1)
+	else if(msg.indexOf("who is ") > -1)
 		{
 		try
 			{
